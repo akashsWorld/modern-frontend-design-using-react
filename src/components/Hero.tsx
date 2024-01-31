94b6262497c7de76b5c17f7eff77cf3d0c7d8b57
akashsWorld <akashbiswas.271999@gmail.com>
2024-01-31 11:57:40 +0530

    The CSS class names are converted in BEN fromat. And convert all the units into rems.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,15 +6,15 @@ import logo_white from "./../assets/logo-white.png";
 const Hero = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={logo_white} alt="logo-white" className="logo" />
+      <div className="header__logo-box">
+        <img src={logo_white} alt="logo-white" className="header__logo" />
       </div>
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
-        <a href="/" className="btn btn-white">Discover our tours</a>
+        <a href="/" className="btn btn--white btn--animation">Discover our tours</a>
       </div>
     </header>
   );
