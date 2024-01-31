4f838dedc5b26e33662aa8896de5c01a027c5264
akashsWorld <akashbiswas.271999@gmail.com>
2024-01-31 23:52:39 +0530

    Change the architecture of the SCSS to 71 the classes are defined into the specific file and folder where they belong to.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './components/Hero'
-import './App.scss'
+import './sass/main.scss'
+
 const App = () => {
   return (
     <div className='body'>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Hero.scss";
+import "./../sass/main.scss";
 
 import logo_white from "./../assets/logo-white.png";
 
